29aecddb8c41ea7a119977ea10c82cbf82de4802
Jitin Tanwar <tanwarjitin@gmail.com>
2020-04-01 09:00:41 +0530

    First commit

---
+++ src/build/precache-manifest.224adcf49e2610a21ebdd317d6eb3857.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "687fade44efda550e31a660c02dcba6b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "630eafb8517b8642be61",
+    "url": "/static/css/main.06feaea5.chunk.css"
+  },
+  {
+    "revision": "119dafdf60bb31112328",
+    "url": "/static/js/2.5e42917f.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/static/js/2.5e42917f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "630eafb8517b8642be61",
+    "url": "/static/js/main.dd013bba.chunk.js"
+  },
+  {
+    "revision": "921ac687539ca31687de",
+    "url": "/static/js/runtime-main.7c2b1bab.js"
+  },
+  {
+    "revision": "f72360bf6c4ae328b99b7214a6b39a71",
+    "url": "/static/media/bg.f72360bf.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
